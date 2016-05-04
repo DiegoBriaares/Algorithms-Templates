4d4027c804065c760d4cbf2342b5be37cd7ee69e
Briaares <briares2698@gmail.com>
2016-05-04 10:17:16 -0500

    Update Suffix Array.cpp

--- Suffix Array.cpp
+++ Suffix Array.cpp
@@ -1,21 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define MAX 100002
-int suffix[MAX];
+#define MAX 262002
 char base[MAX];
 int n;
-int cmp(pair<int,char *>a,pair<int,char *>b){
-	return strcmp(a.second,b.second)<0?1:0;
-}
-void tree(){
-	pair<int,char *>v[MAX];
+int p[200][MAX];
+struct entry {
+    int a,ba,x;
+    bool operator <(const entry &b) const
+        {
+        if(a==b.a) return ba<b.ba;
+        else return a<b.a;
+        }
+} v[MAX];
+void suffixArray(){
 	for(int i=0;i<n;i++){
-		v[i].first=i;
-		v[i].second=(base+i);
+		if(base[i]>='A'&&base[i]<='Z')p[0][i]=base[i]-'A';
+		else p[0][i]=base[i]-'a'+26;
 	}
-	sort(v,v+n,cmp);
-	for(int i=0;i<n;i++){
-		suffix[i]=v[i].first;
+	int uso=1;
+	for(int exp=1;(exp>>1)<n;uso++,exp<<=1){
+		for(int i=0;i<n;i++){
+			v[i].a=p[uso-1][i];
+			v[i].ba=i+exp<n?p[uso-1][i+exp]:-1;
+			v[i].x=i;
+		}
+		sort(v,v+n);
+		for(int i=0;i<n;i++){
+			p[uso][v[i].x]=i>0&&v[i].a==v[i-1].a&&v[i].ba==v[i-1].ba?p[uso][v[i-1].x]:i;
+		}
 	}
 }
 	int main (){
@@ -23,6 +35,6 @@ void tree(){
 		cin.tie(0);
 		cin>>base;
 		n=strlen(base);
-		tree();
-		for(int i=0;i<n;i++)cout << suffix[i]<<"\n";
+		suffixArray();
+		for(int i=0;i<n;i++)cout << v[i].x<<"\n";
 	}
