a6d3349be88c8766861cdc2f3db8d6930117f2af
Briaares <briares2698@gmail.com>
2016-05-06 12:38:52 -0500

    Update Multiplicación (Big Numbers).cpp

--- Multiplicación (Big Numbers).cpp
+++ Multiplicación (Big Numbers).cpp
@@ -1,11 +1,9 @@
 ll go(ll x, ll y){
-	ll r = 0;
-
-	for (; y; y /= 2){
-		if (y % 2)
-			r = (r + x) % m;
-		x = (x + x) % m;
+	ll r=0;
+	for (;y;y/=2){
+		if ((y&1))
+			r=(r+x)%MOD;
+		x=(x+x)%MOD;
 	}
-
 	return r;
 }
